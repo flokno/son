60461dd9db55b446be516734e5ff83c6569dac27
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-06-04 12:25:01 +0200

    remove unnecessary stuff from example

--- examples/illustration.py
+++ examples/illustration.py
@@ -1,11 +1,8 @@
+"""creates a file test.son and reads back in"""
+
 import son
 
 fname = "test.son"
-
-
-def dump(obj, file, **kwargs):
-    return son.dump(obj, file, **kwargs)
-
 
 # Metadata
 m = {"purpose": "store biography data", "version": 0.1}
@@ -15,11 +12,11 @@ d1 = {"first name": "Hildegard", "second name": "Kneef", "age": 93}
 d2 = {"first name": "Wiglaf", "second name": "Droste", "age": 57}
 
 # dump metadata to file
-dump(m, fname, is_metadata=True, indent=2)
+son.dump(m, fname, is_metadata=True, indent=2)
 
 # dump data points to file
 for obj in (d1, d2):
-    dump(obj, fname, indent=2)
+    son.dump(obj, fname, indent=2)
 
 # load back
 metadata, data = son.load(fname)
